d4b804a2269f492d197b08df751691ad05ffa172
buildwithhk <ihustlerharshit@gmail.com>
2026-07-06 03:21:49 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+client = OpenAI(
+    api_key=st.secrets["GEMINI_API_KEY"],
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+import streamlit as st
+
+st.title("Harsh AI Chat")
+st.caption("🚀 A chatbot made by Harshit")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(
+    model="gemini-2.5-flash" , messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
